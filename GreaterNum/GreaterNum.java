151e7152a8eeb8565f587a1d0f03b1cdb15a4e3f
Abirami S <137076840+Abirami76@users.noreply.github.com>
2025-01-22 20:48:26 +0530

    GreaterNum

--- GreaterNum/GreaterNum.java
+++
@@ -1,7 +0,0 @@
-class GreaterNum{
-    public static void main(String[] args){
-       int a=34,b=68,c=72;
-       int g=(a>b&&b>c)?a:(b>c)?b:c;
-       System.out.println("The greatest number is "+g);
-    }
-}
